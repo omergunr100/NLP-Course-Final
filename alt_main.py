5b7592f9efef4337ddd6a20fc788e370d2d9d57c
OE100 <omer.er10@gmail.com>
2024-03-22 22:57:52 +0200

    fixed title algorithm

--- alt_main.py
+++ alt_main.py
@@ -1,18 +1,32 @@
 import json
+import re
 import string
+import time
+import warnings
+from datetime import datetime
 
 import benepar
 import en_core_web_md
 import numpy as np
 import pandas as pd
+import plotly.express as px
 from pandas import DataFrame
 from plotly.graph_objs import Figure
 from sentence_transformers import SentenceTransformer
 from sklearn.decomposition import PCA
 from spacy import Language
-import plotly.express as px
+from spacy.parts_of_speech import AUX, PRON, DET, PART, ADJ
+from spacy.symbols import neg
 
 from compare_clustering_solutions import evaluate_clustering
+
+# hyperparameter dictionary
+hyper_params = {
+    'threshold': 0.756,
+    'batch_size': 200,
+    'max_iterations': 100,
+    'plot_filename': f'clustering_results_{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}'
+}
 
 
 # clean the query text for processing
@@ -51,7 +65,7 @@ def calculate_distances(df: DataFrame):
 # find the closest neighbors for each request
 def find_closest_neighbor_distance(df: DataFrame):
     distance_columns = [col for col in df.columns if '_distance' in col]
-    df['closest_neighbor_distance'] = df.loc[:, df.columns.isin(distance_columns)].min(axis=1)
+    df['closest_neighbor_dist'] = df.loc[:, df.columns.isin(distance_columns)].min(axis=1)
 
 
 # count closest neighbors for each request
@@ -61,7 +75,7 @@ def count_closest_neighbors(df: DataFrame, threshold: float):
     for i in range(len(df)):
         i_idx = df.index[i]
         # find the closest neighbor distance
-        min_distance = df.at[i_idx, 'closest_neighbor_distance']
+        min_distance = df.at[i_idx, 'closest_neighbor_dist']
         # if the distance is greater than the threshold, skip the request
         if min_distance > threshold:
             continue
@@ -88,7 +102,7 @@ def calculate_centroids(df: DataFrame, clusters: dict):
 def cluster_requests(df: DataFrame, clusters: dict, threshold: float, sort_by_count: bool = False) -> int:
     num_changes = 0
 
-    # shuffle the data
+    # shuffle/sort the data
     if sort_by_count:
         order = df.sort_values(by='closest_neighbors', ascending=False).index
     else:
@@ -98,12 +112,13 @@ def cluster_requests(df: DataFrame, clusters: dict, threshold: float, sort_by_co
     max_key = max(clusters.keys(), default=-1) + 1
 
     # iterate over the data
+    batch_size = hyper_params['batch_size']
     for i in range(len(df)):
-        # calculate the centroids every 200 requests
-        if i % 200 == 0:
+        # calculate the centroids every batch size
+        if i % batch_size == 0:
             calculate_centroids(df, clusters)
 
-        # get the row
+        # get the row data
         encoded = df.at[order[i], 'encoded']
         cluster = df.at[order[i], 'cluster']
 
@@ -150,6 +165,7 @@ def cluster_requests(df: DataFrame, clusters: dict, threshold: float, sort_by_co
 # give a title to a cluster
 def title_clusters(df: DataFrame, clusters: dict, nlp: Language, min_size: int):
     df['title'] = 'cluster_' + df['cluster'].astype(str)
+    df['title_form'] = ''
     for cluster in clusters.keys():
         # if the cluster size is below the threshold, set the title to unclustered
         if len(clusters[cluster]['requests']) < min_size:
@@ -157,15 +173,27 @@ def title_clusters(df: DataFrame, clusters: dict, nlp: Language, min_size: int):
             df.loc[df['cluster'] == cluster, 'cluster'] = -1
             continue
 
+        indexing = df['cluster'] == cluster
         # get all sentences in the cluster to a list
-        sentence_list = df.loc[df['cluster'] == cluster, 'text'].to_list()
+        sentence_list = df.loc[indexing, 'text'].to_list()
         # strip the sentences of any extra spaces (anywhere) and periods (at the end)
         sentence_list = [' '.join(sentence.split()).strip('.') for sentence in sentence_list]
-        # concatenate the sentences and make sure that they end with punctuation
-        paragraph = ' '.join([sentence if sentence.endswith(string.punctuation) else sentence + '.'
-                              for sentence in sentence_list])
-        # analyze the concatenated sentences (the entire cluster)
-        doc = nlp(paragraph)
+        # if the sentence is longer than 5 words, cut away less important parts:
+        # filter out all determiners, pronouns, auxiliary verbs, parts that don't contribute much in a short title
+        # if the token is a negation, use the lemma instead of the text (n't -> not)
+        sentence_list = [sentence.strip() if len(sentence.split()) < 6 else
+                         ' '.join([token.text if token.dep != neg else token.lemma_ for token in nlp(sentence)
+                                   if token.pos not in {DET, PRON, AUX, PART, ADJ} or token.dep == neg]).strip()
+                         for sentence in sentence_list]
+
+        # if the sentences don't end with a punctuation mark, add a period
+        sentence_list = [sentence if sentence.endswith(tuple(s for s in string.punctuation)) else sentence + '.'
+                         for sentence in sentence_list]
+
+        df.loc[indexing, 'title_form'] = sentence_list
+
+        # concatenate all the sentences to a single doc
+        doc = nlp(' '.join(sentence_list))
 
         # initialize the max similarity and the max string
         max_sim = float('-inf')
@@ -174,13 +202,14 @@ def title_clusters(df: DataFrame, clusters: dict, nlp: Language, min_size: int):
         # find the single-standalone sentence that is the most similar to all the sentences concatenated
         for sent in doc.sents:
             for constituent in sent._.constituents:
-                if 2 < len(constituent) < 8:
+                if 1 < len(constituent) < 6:
                     if (similarity := doc.similarity(constituent)) > max_sim:
                         max_sim = similarity
                         max_str = constituent.text
 
+        # get rid of any punctuation inside the title or extra spaces
+        title = ' '.join(word for word in re.split("\?|,|\.|!| ", max_str)).strip()
         # set the most similar as the title
-        title = max_str.strip('.')
         if title != '':
             df.loc[df['cluster'] == cluster, 'title'] = title
             clusters[cluster]['title'] = title
@@ -233,7 +262,7 @@ def destroy_below_min_size(df: DataFrame, clusters: dict, min_size: int) -> tupl
 # prepare the spacy pipeline for the naming part of the task
 def prepare_spacy_pipeline() -> Language:
     # load the spaCy model
-    benepar.download('benepar_en3')
+    benepar.download('benepar_en3', quiet=True)
     nlp = en_core_web_md.load()
     # load the benepar parser
     nlp.add_pipe("benepar", config={"model": "benepar_en3"})
@@ -242,10 +271,10 @@ def prepare_spacy_pipeline() -> Language:
 
 
 # reduces the dimensionality of the data to k dimensions
-def reduce_dimensions(df: DataFrame, dims: int):
-    pca = PCA(n_components=dims)
+def reduce_dimensions(df: DataFrame):
+    pca = PCA(n_components=3)
     reduced = pca.fit_transform(df['encoded'].to_list())
-    for i in range(dims):
+    for i in range(3):
         df[f'reduced{str(i)}'] = reduced[:, i]
 
 
@@ -260,6 +289,7 @@ def plot_results(df: DataFrame,
                         hover_data={
                             'text': True,
                             'title': True,
+                            'title_form': True,
                             x: True,
                             y: True,
                             z: True
@@ -270,6 +300,9 @@ def plot_results(df: DataFrame,
 
 
 def analyze_unrecognized_requests(data_file, output_file, min_size):
+    # todo: remove timing code
+    start = time.time()
+
     # read the data
     df = read_lines(data_file)
 
@@ -279,41 +312,51 @@ def analyze_unrecognized_requests(data_file, output_file, min_size):
     # encode the sentences
     encode_sentences(df, model)
 
-    # calculate the distances
-    calculate_distances(df)
+    # ignore performance warning
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
 
-    # find the closest neighbors for each request
-    find_closest_neighbor_distance(df)
+        # calculate the distances
+        calculate_distances(df)
 
-    # count closest neighbors for each request
-    count_closest_neighbors(df, 0.756)
+        # find the closest neighbors for each request
+        find_closest_neighbor_distance(df)
+
+        # count closest neighbors for each request
+        count_closest_neighbors(df, hyper_params['threshold'])
+
+    # drop now useless columns
+    df.drop(columns=[column for column in df.columns if '_distance' in column], inplace=True)
 
     # initialize the clusters dictionary
     clusters = dict()
-    max_iterations = 100
+    max_iterations = hyper_params['max_iterations']
     # initial values to prevent errors
-    num_changes = 1
+    num_changes = None
     iteration = None
     destroy_below_threshold = True
+    sort = True
     # cluster the requests
     for iteration in range(max_iterations):
         # cluster the requests
         num_changes = cluster_requests(df=df,
                                        clusters=clusters,
-                                       threshold=0.756,
-                                       sort_by_count=iteration % 5 == 0)
+                                       threshold=hyper_params['threshold'],
+                                       sort_by_count=sort)
+        sort = False
         print(f"Number of changes: {num_changes}")
 
-        # remove clusters below the minimum size the first time the number of changes is 0
+        # remove clusters below the minimum size the first time the number of changes is below the threshold
         if num_changes == 0 and destroy_below_threshold and iteration < max_iterations - 1:
             destroy_below_threshold = False
+            sort = True
             num_clusters, num_requests = destroy_below_min_size(df, clusters, int(min_size))
             print(f"Removed {num_clusters} clusters below the minimum size, with a total of {num_requests} requests.")
             num_changes = num_requests
 
         print(f"Iteration {iteration + 1} completed\n")
 
-        # if the number of changes is 0, the algorithm has converged
+        # if the number of changes is within threshold, the algorithm has converged
         if num_changes == 0:
             break
 
@@ -321,6 +364,7 @@ def analyze_unrecognized_requests(data_file, output_file, min_size):
         print(f"Converged after {iteration + 1} iterations.\n")
     else:
         print(f"Did not converge after {max_iterations} iterations, maximum number of iterations reached.\n")
+    print(f"Clustering time: {round(time.time() - start)} seconds.")
 
     # prepare the spacy pipeline
     nlp = prepare_spacy_pipeline()
@@ -331,15 +375,18 @@ def analyze_unrecognized_requests(data_file, output_file, min_size):
     # transform the clusters into the required format
     result = transform_clusters_dict(df, clusters, int(min_size))
 
-    # reduce the dimensions of the data for visualization
-    reduce_dimensions(df, 3)
+    if create_plot := True:
+        # create a 3D representation of the clusters for visualization
+        reduce_dimensions(df)
 
-    # plot the results
-    fig = plot_results(df, title='Clustering Results', axis=('reduced0', 'reduced1', 'reduced2'), color='title')
-    fig.write_html(f'clustering_results.html')
+        # plot the results
+        fig = plot_results(df, title='Clustering Results', axis=('reduced0', 'reduced1', 'reduced2'), color='title')
+        fig.write_html(f'{hyper_params["plot_filename"]}.html')
 
     # output the result to a json file
     output_result(result, output_file)
+
+    print(f"Total time: {round(time.time() - start)} seconds.\n")
 
 
 if __name__ == '__main__':
